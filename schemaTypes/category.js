6b295dffc32e2ecdeb4023f98f957d3d307af447
X001PM <97795272+tabzyx@users.noreply.github.com>
2026-03-27 00:46:20 +0500

    Update category.js

--- schemaTypes/category.js
+++ schemaTypes/category.js
@@ -6,11 +6,13 @@ export default {
     {
       name: 'title',
       type: 'string',
+      validation: Rule => Rule.required(),
     },
     {
       name: 'slug',
       type: 'slug',
       options: { source: 'title' },
+      validation: Rule => Rule.required(),
     },
   ],
 }
