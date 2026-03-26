0c091ea484c4d766e418abbddf016ade52f31059
X001PM <97795272+tabzyx@users.noreply.github.com>
2026-03-26 23:03:43 +0500

    Add files via upload

---
+++ schemaTypes/index.js
@@ -0,0 +1,17 @@
+import article from './article'
+import category from './category'
+import industry from './industry'
+import service from './service'
+import tag from './tag'
+import author from './author'
+import download from './download'
+
+export const schemaTypes = [
+  article,
+  category,
+  industry,
+  service,
+  tag,
+  author,
+  download
+]
