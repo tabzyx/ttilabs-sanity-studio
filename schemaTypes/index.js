182a38e6f3a2c5eb11c6531ebec81392dc6de45f
X001PM <97795272+tabzyx@users.noreply.github.com>
2026-03-26 23:28:58 +0500

    Remove unused schema types from index.js

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,17 +1,4 @@
 import article from './article'
 import category from './category'
-import industry from './industry'
-import service from './service'
-import tag from './tag'
-import author from './author'
-import download from './download'
 
-export const schemaTypes = [
-  article,
-  category,
-  industry,
-  service,
-  tag,
-  author,
-  download
-]
+export const schemaTypes = [article, category]
