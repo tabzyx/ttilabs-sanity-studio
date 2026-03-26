9829cc62908b31d3052d5b9c7f3b1d0a5bb9eff9
X001PM <97795272+tabzyx@users.noreply.github.com>
2026-03-27 01:44:11 +0500

    Update tag.js

--- schemaTypes/tag.js
+++ schemaTypes/tag.js
@@ -8,5 +8,11 @@ export default {
       type: 'string',
       validation: Rule => Rule.required(),
     },
+    {
+      name: 'slug',
+      type: 'slug',
+      options: { source: 'title' },
+      validation: Rule => Rule.required(),
+    },
   ],
 }
