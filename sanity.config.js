4181418ea414440bc12486e5fe307811e1b9656c
X001PM <97795272+tabzyx@users.noreply.github.com>
2026-03-26 22:36:31 +0500

    Add initial Sanity configuration file

---
+++ sanity.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'TTI KB',
+
+  projectId: 'tma5c4o6', // 👈 YOUR PROJECT ID
+  dataset: 'production',
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
